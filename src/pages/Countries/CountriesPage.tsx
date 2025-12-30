1040893bddefd596ed7d2038d15043737c17ffe0
Sindhura Gollamudi <gollamudisindhura1@gmail.com>
2025-12-29 22:11:37 -0500

    cleared the errors in the CountriesPage

--- src/pages/Countries/CountriesPage.tsx
+++ src/pages/Countries/CountriesPage.tsx
@@ -2,20 +2,39 @@ import { Link } from "react-router-dom";
 import { useFetch } from "../../hooks/useFetch";
 import "./countries.css";
 
+
+type Country = {
+  strArea: string;
+};
+
+type CountriesResponse = {
+  meals: Country[];
+};
+
 export default function CountriesPage() {
-  const { data, loading } = useFetch(
+  const { data, loading } = useFetch<CountriesResponse>(
     "https://www.themealdb.com/api/json/v1/1/list.php?a=list"
   );
 
-  if (loading) return <p>Loading...</p>;
+  if (loading) return <p>Loading countries...</p>;
+  //if (Error) return <p>{Error}</p>;
+  if (!data) return null;
 
   return (
     <div className="countries fade-in">
-      {data.meals.map((c) => (
-        <Link key={c.strArea} to={`/country/${c.strArea}`}>
-          {c.strArea}
-        </Link>
-      ))}
+      <h2>Browse Recipes by Country</h2>
+
+      <div className="countries-grid">
+        {data.meals.map((country) => (
+          <Link
+            key={country.strArea}
+            to={`/country/${country.strArea}`}
+            className="country-card"
+          >
+            {country.strArea}
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
